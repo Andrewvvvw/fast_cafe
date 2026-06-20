06fa74dba0e97b5a723245fb15484da0eb839860
Andrewvvvw <andrewvvvw@gmail.com>
2026-06-20 10:45:02 +0300

    data: expand menu items list for initial database seeding

--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,9 @@ async def seed_cafe_menu(session: AsyncSession):
         cappucino = MenuItem(name="Каппучино", price=Decimal("3.00"), is_available=True)
         latte = MenuItem(name="Латте", price=Decimal("3.50"), is_available=True)
         croissant = MenuItem(name="Круассан", price=Decimal("2.50"), is_available=True)
+        cappucino1 = MenuItem(name="Каппучино2", price=Decimal("3.00"), is_available=True)
+        latte1 = MenuItem(name="Латте2", price=Decimal("3.50"), is_available=True)
+        croissant1 = MenuItem(name="Круассан2", price=Decimal("2.50"), is_available=True)
 
-        session.add_all([cappucino, latte, croissant])
+        session.add_all([cappucino, latte, croissant, cappucino1, latte1, croissant1])
         await session.commit()
